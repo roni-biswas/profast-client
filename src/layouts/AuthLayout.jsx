2097f3c06d1ce398a22d5c024bf40015e5763a49
Roni Biswas <ronibiswas068@gmail.com>
2025-06-21 20:48:06 +0600

    integrated basic react-hook-form

--- src/layouts/AuthLayout.jsx
+++ src/layouts/AuthLayout.jsx
@@ -5,10 +5,10 @@ import ProFastLogo from "../pages/shared/ProFastLogo/ProFastLogo";
 
 const AuthLayout = () => {
   return (
-    <div className="max-w-7xl mx-auto px-4 md:px-12 lg:px-16 xl:px-24">
+    <div className="max-w-7xl mx-auto px-4 md:px-12 lg:px-16 xl:px-24 ">
       <div className="p-12 bg-base-200">
         <ProFastLogo />
-        <div className="hero-content flex-col lg:flex-row-reverse">
+        <div className="hero-content flex-col lg:flex-row-reverse mt-4 items-center ">
           <div className="flex-1">
             <img src={authImg} className="max-w-sm" />
           </div>
--- src/pages/Authentication/Login/Login.jsx
+++ src/pages/Authentication/Login/Login.jsx
@@ -1,9 +1,35 @@
 import React from "react";
+import { useForm } from "react-hook-form";
 
 const Login = () => {
+  const { register, handleSubmit } = useForm();
+
+  const onSubmit = (data) => console.log(data);
+
   return (
-    <div>
-      <h1>This is Login Page</h1>
+    <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+      <div className="card-body">
+        <form onSubmit={handleSubmit(onSubmit)} className="fieldset">
+          <label className="label">Email</label>
+          <input
+            type="email"
+            {...register("email")}
+            className="input"
+            placeholder="Email"
+          />
+          <label className="label">Password</label>
+          <input
+            type="password"
+            {...register("password")}
+            className="input"
+            placeholder="Password"
+          />
+          <div>
+            <a className="link link-hover">Forgot password?</a>
+          </div>
+          <button className="btn btn-neutral mt-4">Login</button>
+        </form>
+      </div>
     </div>
   );
 };

162e576ee69c2e453bf3c124ef89e9a924952db5
Roni Biswas <ronibiswas068@gmail.com>
2025-06-22 12:28:32 +0600

    integrated register page and implemented get value and validation using  pkg react-form-hook

--- src/pages/Authentication/Login/Login.jsx
+++ src/pages/Authentication/Login/Login.jsx
@@ -2,28 +2,44 @@ import React from "react";
 import { useForm } from "react-hook-form";
 
 const Login = () => {
-  const { register, handleSubmit } = useForm();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
 
   const onSubmit = (data) => console.log(data);
 
   return (
     <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
       <div className="card-body">
+        <h1 className="text-2xl font-bold">Login Now</h1>
         <form onSubmit={handleSubmit(onSubmit)} className="fieldset">
           <label className="label">Email</label>
           <input
             type="email"
-            {...register("email")}
+            {...register("email", { required: true })}
             className="input"
             placeholder="Email"
           />
+          {errors.email?.type === "required" && (
+            <p className="text-red-500">Email is Required</p>
+          )}
           <label className="label">Password</label>
           <input
             type="password"
-            {...register("password")}
+            {...register("password", { required: true, minLength: 6 })}
             className="input"
             placeholder="Password"
           />
+          {errors.password?.type === "required" && (
+            <p className="text-red-500">Password is Required</p>
+          )}
+          {errors.password?.type === "minLength" && (
+            <p className="text-red-500">
+              Password must be 6 Character's or longer
+            </p>
+          )}
           <div>
             <a className="link link-hover">Forgot password?</a>
           </div>
---
+++ src/pages/Authentication/Register/Register.jsx
@@ -0,0 +1,71 @@
+import React from "react";
+import { useForm } from "react-hook-form";
+
+const Register = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = (data) => console.log(data);
+  return (
+    <div className="card bg-base-100 w-full md:max-w-sm shrink-0 shadow-2xl">
+      <div className="card-body">
+        <h1 className="text-2xl font-bold">Create an Account Now</h1>
+        <form onSubmit={handleSubmit(onSubmit)} className="fieldset">
+          {/* Name Field */}
+          <label className="label">Fullname</label>
+          <input
+            type="text"
+            {...register("fullName", { required: true })}
+            className="input"
+            placeholder="Fullname"
+          />
+          {errors.fullName?.type === "required" && (
+            <p className="text-red-500">Full Name is Required</p>
+          )}
+          {/* Email Field */}
+          <label className="label">Email</label>
+          <input
+            type="email"
+            {...register("email", { required: true })}
+            className="input"
+            placeholder="Email"
+          />
+          {errors.email?.type === "required" && (
+            <p className="text-red-500">Email is Required</p>
+          )}
+          {/* Password Field */}
+          <label className="label">Password</label>
+          <input
+            type="password"
+            {...register("password", {
+              required: true,
+              minLength: {
+                value: 8,
+                message: "Password must be at least 8 characters",
+              },
+              pattern: {
+                value: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).+$/,
+                message:
+                  "Password must include uppercase, lowercase, and a digit",
+              },
+            })}
+            className="input"
+            placeholder="Password"
+          />
+          {errors.password && (
+            <p className="text-red-500">{errors.password.message}</p>
+          )}
+          <div>
+            <a className="link link-hover">Forgot password?</a>
+          </div>
+          <button className="btn btn-neutral mt-4">Register</button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Register;
